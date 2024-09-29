8468f3993ddafc9bf290c1b38638f777bc8dee7e
SheeTabz <wmuriithitabitha@gmail.com>
2024-09-29 10:40:16 +0300

    Added the other components into the main component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navBar';
+import RaceAddForm from './components/RaceModule/RaceAddForm';
+import StudentAddForm from './components/Students/StudentAddForm';
+import RaceParticipantForm from './components/RaceModule/RaceParticipantForm';
+import RaceDetails from './components/RaceModule/RaceDetails';
+import StudentList from './components/Students/StudentList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='bg-blue-800 text-white' >
+<NavBar></NavBar>
       </header>
+
+      {/* <RaceAddForm></RaceAddForm> */}
+      {/* <StudentAddForm></StudentAddForm> */}
+      {/* <RaceParticipantForm></RaceParticipantForm> */}
+      {/* <RaceDetails></RaceDetails> */}
+      <StudentList></StudentList>
     </div>
   );
 }
