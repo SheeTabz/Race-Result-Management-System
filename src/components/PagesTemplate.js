72db75ff0fbd477c06a307dbbf0ce04e1fa8a92e
SheeTabz <wmuriithitabitha@gmail.com>
2024-10-01 17:43:31 +0300

    Page header template component

--- src/components/PagesTemplate.js
+++ src/components/PagesTemplate.js
@@ -1,23 +1,16 @@
 import React from 'react'
-// import {track} from "public/track.jpg"
-// import { Link } from 'react-router-dom';
+
 
 function PagesTemplate({pageData}) {
   return (
-    <div className='hero h-[25vh] lg:h-[25vh] text-white w-full'>
+    <div className='hero xs:h-[15vh]  h-[10vh] lg:h-[25vh] text-white w-full'>
     <div className='  h-full w-full bg-no-repeat bg-cover  bg-[url("https://img.freepik.com/premium-photo/running-track_629524-11583.jpg?w=1380")]'>
     <div className='review h-full'>
-    <div className=' relative flex flex-col justify-center items-center' >
-      <div className=' space-y-6 flex flex-col items-center justify-center space-x-3 p-4  max-w-max px-20 py-10'>
-    
-        <h1 className='text-7xl title font-["Shadows Into Light"] '>{pageData}</h1>
-         <p className='text-3xl font-light'>
-          Home .  </p>
-       
-         
-        
+    {/* <div className=' relative flex flex-col justify-center items-center' > */}
+      <div className=' flex flex-col items-center justify-center align-center smax-w-max px-20 py-10'>
+        <h1 className=' xs:text-2xl md:text-5xl title font-["Shadows Into Light"] '>{pageData}</h1>
       </div>
-     </div>
+     {/* </div> */}
      </div>
      </div>
  </div>
