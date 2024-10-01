2c64dd872c6db0d82f2ece0950179c19ca636ee8
SheeTabz <wmuriithitabitha@gmail.com>
2024-10-01 17:48:00 +0300

    Edited the app.js to Render all necessary components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navBar';
-import RaceAddForm from './components/RaceModule/RaceAddForm';
-import StudentAddForm from './components/Students/StudentAddForm';
-import RaceParticipantForm from './components/RaceModule/RaceParticipantForm';
 import RaceDetails from './components/RaceModule/RaceDetails';
 import StudentList from './components/Students/StudentList';
-
+import { useState, useEffect } from 'react';
+import RaceList from './components/RaceModule/RaceList';
+import { Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 function App() {
-  return (
-    <div className="App">
-      <header className='bg-blue-800 text-white' >
-<NavBar></NavBar>
-      </header>
 
-      {/* <RaceAddForm></RaceAddForm> */}
-      {/* <StudentAddForm></StudentAddForm> */}
-      {/* <RaceParticipantForm></RaceParticipantForm> */}
-      {/* <RaceDetails></RaceDetails> */}
-      <StudentList></StudentList>
+  // const dispatch = useDispatch();
+
+  // // Load initial students data
+  // useEffect(() => {
+  //   dispatch(setStudents(studentData));
+  // }, [dispatch]);
+
+  return (
+    <Provider store={store}>
+    <div className="App">
+      <header className=' nav-header bg-blue-800 text-white' >
+<NavBar />
+<hr className='bg-black' />
+      </header>
+      <Routes>   
+ 
+      <Route path='/' exact element={<RaceList />} />
+     
+      <Route path='/races' element={<RaceList />} />
+      <Route path='/students' element={<StudentList  />} />
+
+   <Route path={"/races/:id"} element={<RaceDetails  />}/>
+   </Routes> 
     </div>
+    </Provider>
   );
 }
 
